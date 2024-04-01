3e2bf7c27513cc26b4c36be8877590ca35d47b00
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-04-01 18:53:11 +0300

    Display first question

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 class Quiz {
-    constructor(questions){}
+    constructor(questions){
+        this.questions = questions;
+        this.questionContainer = document.getElementById('question');
+        this.answerButtons = document.getElementById('answer-buttons');
+        this.feedback = document.getElementById('feedback');
+        this.nextButton = document.getElementById('next-button');
+        this.showQuestion(this.questions[0]);
+
+    }
+
+    showQuestion(question) {
+        this.questionContainer.textContent = question.question;
+        this.answerButtons.innerHTML = '';
+        for (const answer of question.answers) {
+            //const answerElement = `<li><button class="btn"></button></li>`;
+            const buttonContainer = document.createElement('li');
+            const buttonElement = document.createElement('button');
+            buttonElement.classList.add('btn');
+            buttonElement.textContent = answer.text;
+            this.answerButtons.appendChild(buttonContainer);
+            buttonContainer.appendChild(buttonElement);
+
+        }
+    }
 }
+
+async function initQuiz() {
+    const fileContent = await fetch('./questions.json');
+    const quizQuestions = await fileContent.json();
+    console.log(quizQuestions);
+    const quiz = new Quiz(quizQuestions);
+}
+
+initQuiz();
+
